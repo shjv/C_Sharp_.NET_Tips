56c751946c1a202e2bb3c857404da86871ca5831
shahrzad javaheri <sh.javaheri.en@gmail.com>
2023-08-06 20:36:56 +0330

    ClassVsStruct

---
+++ src/ClassVsStruct/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassVsStruct
+{
+    struct Point
+    {
+        public int x;
+        public int y;
+
+        // structs are value types and classes are reference types
+
+        //does not compile - struct can't have
+        //explicit parameterless constuctor
+        //public Point()
+        //{
+
+        //}
+
+        //does not compile - all fields must be
+        //assigned in the constructor
+        //public Point(int x)
+        //{
+
+        //}
+
+        //does not compile - structs can't have destructors
+        //public ~Point() 
+        //{
+
+        //}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.ReadKey();
+        }
+    }
+}
